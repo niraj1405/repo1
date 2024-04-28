837c0f0950160320432ad4ed4f843a8781e5e3b8
niraj1405 <nirajkumarverma.78@gmail.com>
2024-04-28 15:35:15 +0530

    Commited

---
+++ KubService/src/main/java/com/app/k8/KubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.k8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubServiceApplication.class, args);
+	}
+
+}
---
+++ KubService/src/test/java/com/app/k8/KubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.k8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
